5d6f1f148bb9f09d561f6dd33059f270a55bd9ac
Néo Ascalon <ignatdominic@yahoo.com>
2019-11-06 08:56:08 +0100

    0.01.1

--- EchecsJavatoC++/CPiece.cpp
+++ EchecsJavatoC++/CPiece.cpp
@@ -1,5 +1,27 @@
 #include "CPiece.h"
 
+void CPiece::situer(int, int)
+{
+}
+
+void CPiece::Deplacer(int, int)
+{
+}
+
+bool CPiece::verifier(int, int)
+{
+	return false;
+}
+
+bool CPiece::trajectoire(int, int)
+{
+	return false;
+}
+
+void CPiece::placer(int, int)
+{
+}
+
 CPiece::CPiece(Couleur Coul, CEchequier &ech)
 {
 
--- EchecsJavatoC++/CPiece.h
+++ EchecsJavatoC++/CPiece.h
@@ -6,14 +6,18 @@ enum Couleur { Noir, Rouge };
 class CPiece
 {
 public:
-
+	void situer(int, int);
+	void Deplacer(int,int);
+	bool verifier(int, int);
+	bool trajectoire(int, int);
+	void placer(int, int);
 	CPiece(Couleur, CEchequier&);
 	~CPiece();
 
 private:
 	CEchequier ech;
 	char rep;
-	int x;
-	int y;
+	int x = -1;
+	int y = -1;
 
 };
